f643a2fb8de9b8de849500555a84d1dcb6d5e53f
jorgekean <jkn379@gmail.com>
2022-12-21 20:55:33 +0800

    initial

---
+++ app/conference/speakers/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Page() {
+
+    return (
+        <>
+            <h1>Conference/speakers Page</h1>
+        </>
+    )
+}
---
+++ app/settings/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Page() {
+
+    return (
+        <>
+            <h1>Settings Page</h1>
+        </>
+    )
+}
